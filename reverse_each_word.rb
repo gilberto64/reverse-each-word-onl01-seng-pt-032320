5714348f2c921f34a6c647891c1b92ee4517455a
Gilberto Ocampo <ocampo@gate.net>
2020-03-25 21:50:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,9 +12,9 @@
 def reverse_each_word(str)
   str2 = " "
   arr2 = str.split
-  arr2.collect do |palabra| palabra.reverse
-  str2 = arr2.join(" ")
+  arr2.collect do |palabra| << palabra.reverse
+  str2 = arr2.join(" ") 
   end 
-  
+  str2 
  end
 
